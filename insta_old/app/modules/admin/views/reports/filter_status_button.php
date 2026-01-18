ee1704a55d9bd819a3859cfd2d3113daa443aa3d
ishagolakiya6101 <ishagolakiya1@gmail.com>
2026-01-18 17:25:24 +0100

    Apply .gitignore and remove ignored files

--- insta_old/app/modules/admin/views/reports/filter_status_button.php
+++
@@ -1,8 +0,0 @@
-<ul class="list-inline mb-0 order_btn_group">
-  <?php 
-    foreach ($report_filters as $key => $item_field) {
-      $class_item = ($task == $key) ? 'btn-primary' : '';
-      echo sprintf('<li class="list-inline-item"><a class="btn %s" href="?type=%s">%s</a>', $class_item, $key, $item_field['name']);
-    }
-  ?>
-</ul>
--- old_files_backup/install/install.php
+++
@@ -1,8 +0,0 @@
-<?php
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require_once('install.class.php');
-$install = new Install();
-$install->go();
