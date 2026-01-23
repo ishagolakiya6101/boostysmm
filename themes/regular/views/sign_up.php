cee983834af2b936b816aea6def130f197e84d2d
ishagolakiya6101 <ishagolakiya1@gmail.com>
2026-01-23 21:52:38 +0530

    sign_up changes

--- themes/regular/views/sign_up.php
+++ themes/regular/views/sign_up.php
@@ -5,20 +5,25 @@
 <div class="login-bg-image"></div>
 <div class="page auth-login-form">
   <div class="container h-100">
-    <div class="row">
-      <div class="col-md-6 col-login mx-auto auth-form">
-        <form class="card actionForm" action="<?=cn("auth/ajax_sign_up")?>" data-redirect="<?=cn('statistics')?>" method="POST">
-          <div class="card-body p-t-10">
+    <div class="card login-card">
+      <div class="row no-gutters">
+        <div class="col-md-6 auth-left">
+          <div class="auth-left-inner">
+            <a href="<?=cn();?>" class="auth-logo">
+              <img src="<?=get_option('website_logo', BASE."assets/images/logo.png")?>" alt="logo" class="logo-lg">
+            </a>
+            <h2 class="auth-headline">Grow your social media with BoostySMM</h2>
+            <p class="auth-subtext">Fast, affordable & <h4>trusted</h4> SMM services</p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card-body">
             <div class="card-title text-center">
-              <div class="site-logo mb-3">
-                <a href="<?=cn()?>">
-                  <img src="<?=get_option('website_logo', BASE."assets/images/logo.png")?>" alt="website-logo" class="website-logo">
-                </a>
-              </div>
               <h5><?=lang("register_now")?></h5>
             </div>
-            <div class="form-group">
-             <div class="row">
+            <form class="actionForm" action="<?=cn("auth/ajax_sign_up")?>" data-redirect="<?=cn('statistics')?>" method="POST">
+              <div class="form-group">
+               <div class="row">
               <!-- <div class="col-md-6">
                   <div class="input-icon mb-3">
                     <span class="input-icon-addon">
@@ -98,33 +103,34 @@
                   <?php }}?>
                 </select>
               </div> -->
-            </div>
-            
-            <?php
-              if (get_option('enable_goolge_recapcha') &&  get_option('google_capcha_site_key') != "" && get_option('google_capcha_secret_key') != "") {
-            ?>
-            <div class="form-group">
-              <div class="g-recaptcha" data-sitekey="<?=get_option('google_capcha_site_key')?>"></div>
-            </div>
-            <?php } ?> 
+              </div>
+              
+              <?php
+                if (get_option('enable_goolge_recapcha') &&  get_option('google_capcha_site_key') != "" && get_option('google_capcha_secret_key') != "") {
+              ?>
+              <div class="form-group">
+                <div class="g-recaptcha" data-sitekey="<?=get_option('google_capcha_site_key')?>"></div>
+              </div>
+              <?php } ?> 
 
-            <div class="form-group">
-              <label class="custom-control custom-checkbox">
-                <input type="checkbox" name="terms" class="custom-control-input" />
-                <span class="custom-control-label"><?=lang("i_agree_the")?> <a href="<?=cn('terms')?>"><?=lang("terms__policy")?></a></span>
-              </label>
-            </div>
-            <div class="">
-              <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
-              <button type="submit" class="btn btn-primary btn-block"><?=lang("create_new_account")?></button>
-            </div>
+              <div class="form-group">
+                <label class="custom-control custom-checkbox">
+                  <input type="checkbox" name="terms" class="custom-control-input" />
+                  <span class="custom-control-label"><?=lang("i_agree_the")?> <a href="<?=cn('terms')?>"><?=lang("terms__policy")?></a></span>
+                </label>
+              </div>
+              <div class="">
+                <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+                <button type="submit" class="btn btn-primary btn-block"><?=lang("create_new_account")?></button>
+              </div>
+            </form>
 
             <?php echo $social_login_html; ?>
-            
+
+            <div class="text-center text-muted">
+              <?=lang("already_have_account")?> <a href="<?=cn('auth/login')?>"><?=lang("Login")?></a>
+            </div>
           </div>
-        </form>
-        <div class="text-center text-muted">
-          <?=lang("already_have_account")?> <a href="<?=cn('auth/login')?>"><?=lang("Login")?></a>
         </div>
       </div>
     </div>
